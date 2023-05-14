51f80e9f13b7786933a7eac3f13de5b68d171f86
Melanie Voloshynska <m.voloshynska@gmail.com>
2023-05-14 21:40:33 +0300

    adiing get_instance method to code

--- FridgeClass/Fridge/Fridge.py
+++ FridgeClass/Fridge/Fridge.py
@@ -1,10 +1,18 @@
 class Fridge:
+    instance = None
+
     def __init__(self, brand, model, capacity, is_defrosting, energy_efficiency_class):
         self.brand = brand
         self.model = model
         self.capacity = capacity
         self.is_defrosting = is_defrosting
         self.energy_efficiency_class = energy_efficiency_class
+
+    @staticmethod
+    def get_instance():
+        if Fridge.instance is None:
+            Fridge.instance = Fridge("Samsung", "RF28R7351SR", 32, True, "A")
+        return Fridge.instance
 
     def turn_on_defrosting(self):
         self.is_defrosting = True
@@ -17,13 +25,38 @@ class Fridge:
 
 
 def main():
-    my_fridge = Fridge("Samsung", "RF28R7351SR", 32, True, "A")
+    fridge1 = Fridge("Samsung", "RF28R7351SR", 32, True, "A")
+    fridge2 = Fridge("LG", "LRMVC2306D", 23, False, "B")
+    fridge3 = Fridge.get_instance()
+    fridge4 = Fridge.get_instance()
 
-    print("Brand:", my_fridge.brand)
-    print("Model:", my_fridge.model)
-    print("Capacity (liters):", my_fridge.capacity)
-    print("Defrosting Enabled:", my_fridge.is_defrosting)
-    print("Energy Efficiency Class:", my_fridge.energy_efficiency_class)
+    print("Brand:", fridge1.brand)
+    print("Model:", fridge1.model)
+    print("Capacity (liters):", fridge1.capacity)
+    print("Defrosting Enabled:", fridge1.is_defrosting)
+    print("Energy Efficiency Class:", fridge1.energy_efficiency_class)
+    print(" ")
+
+    print("Brand:", fridge2.brand)
+    print("Model:", fridge2.model)
+    print("Capacity (liters):", fridge2.capacity)
+    print("Defrosting Enabled:", fridge2.is_defrosting)
+    print("Energy Efficiency Class:", fridge2.energy_efficiency_class)
+    print(" ")
+
+    print("Brand:", fridge3.brand)
+    print("Model:", fridge3.model)
+    print("Capacity (liters):", fridge3.capacity)
+    print("Defrosting Enabled:", fridge3.is_defrosting)
+    print("Energy Efficiency Class:", fridge3.energy_efficiency_class)
+    print(" ")
+
+    print("Brand:", fridge4.brand)
+    print("Model:", fridge4.model)
+    print("Capacity (liters):", fridge4.capacity)
+    print("Defrosting Enabled:", fridge4.is_defrosting)
+    print("Energy Efficiency Class:", fridge4.energy_efficiency_class)
+    print(" ")
 
 
 if __name__ == "__main__":
